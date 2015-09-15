54450f2a0d6f957edab9d2d423a0d998a5d5f270
818macpro <a818@818macprodeMacBook-Pro.local>
2015-09-15 17:24:00 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 818macpro on 9/15/15.
+//  Copyright (c) 2015 ken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
